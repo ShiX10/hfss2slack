3e268f9e9063988ffc59fa937b644d5f2fdecbb9
shiiba <shiiba@cs.kumamoto-u.ac.jp>
2025-08-28 23:31:58 +0900

    edit: add comments in config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -4,13 +4,18 @@
 OS="W"
 
 #HFSS
+#If license is shared with other PC ("True" or "False")
 SHARED_LICENSE="True"
+#When $SHARED_LICENSE is “True”, edit the three lines below.
 LICENSE_SERV="IP address of license server"
 SSH_ACCOUNT="account_name@$LICENSE_SERV"
 LMSTAT_CMD="path_of/lmutil lmstat -c 1055@$LICENSE_SERV -A"
 
+#Full path of HFSS
 RUN_HFSS="path_of\ansysedtsv.exe"
+#Your working directory for HFSS (on Windows, set a path that works for everyone)
 WORK_DIR="Your HFSS Directory"
+#Keywords for extaracting log
 LOG_KEYWORDS=$(printf "%s|" \
     "Pass Number:" \
     "Parametric Analysis is done" \
@@ -20,10 +25,13 @@ LOG_KEYWORDS=$(printf "%s|" \
 LOG_KEYWORDS=${LOG_KEYWORDS%|}
 
 #SLACK
+#Channel id
 SLACK_CHANNEL_ID="Cxxxxxxxxxx"
+#Slack bot token
 SLACK_BOT_TOKEN="xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#Send job log to Slack ("True" or "False")
 SEND_LOG="True"
 
-#YAD (Size of dialogue windows)
+#YAD (Size of dialogue windows for hfsssub.sh)
 YADHEIGHT=200
 YADWIDTH=400
